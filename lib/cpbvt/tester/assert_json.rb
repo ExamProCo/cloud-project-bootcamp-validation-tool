49cda92cba9fa9e832f1faa40d7732d77498713d
Andrew Brown <andrew@monsterboxpro.com>
2023-06-23 22:38:09 +0000

    assert json

--- lib/cpbvt/tester/assert_json.rb
+++ lib/cpbvt/tester/assert_json.rb
@@ -1,26 +1,127 @@
 class AssertJson
   def initialize describe_key:, spec_key:, report:,data:, keys:
+    value = data
     keys.each do |key|
-      data = data[key]
+      if value.key(key)
+        value = value[key]
+      else
+        value = nil
+        break
+      end
     end
-    @data = data
+    @value = value
     return self
   end
 
   def expects_eq value
-    @data == value
+    if @data == value
+      self.pass!(
+        kind: 'assert_json:expects_eq', 
+        status: :pass, 
+        message: 'value was equal to', 
+        data: { 
+          provided_value: value,
+          actual_value: @value
+        }
+      )
+    else
+      self.fail!(
+        kind: 'assert_json:expects_eq',
+        status: :fail, 
+        message: 'value was not equal to', 
+        data: { 
+          provided_value: value,
+          actual_value: @value
+        }
+      )
+    end
   end
 
-  def expects_gt number
-    @data > number
+  def expects_gt value
+    if @value > value
+      self.pass!(
+        kind: 'assert_json:expects_gt', 
+        status: :pass, 
+        message: 'value was greater than', 
+        data: { 
+          provided_value: value,
+          actual_value: @value
+        }
+      )
+    else
+      self.fail!(
+        kind: 'assert_json:expects_match',
+        status: :fail, 
+        message: 'value was not greater than', 
+        data: { 
+          provided_value: value,
+          actual_value: @value
+        }
+      )
+    end
   end
 
   def expects_match value
-    @data.match(value)
+    if @value.match(value)
+      self.pass!(
+        kind: 'assert_json:expects_match', 
+        status: :pass, 
+        message: 'matched as expected', 
+        data: { 
+          provided_value: value,
+          actual_value: @value
+        }
+      )
+    else
+      self.fail!(
+        kind: 'assert_json:expects_match',
+        status: :fail, 
+        message: 'failed to match', 
+        data: { 
+          provided_value: value,
+          actual_value: @value
+        }
+      )
+    end
   end
 
-  def returns key=nil
-    return @data if key == :all || key.nil?
-    return @data[key]
+  def returns key
+    data = @data
+    if key == :all || key.nil?
+      self.fail! kind: 'assert_json:return', status: :pass, message: 'return all data'
+      return data 
+    end
+    if data.key?(key)
+      self.fail! kind: 'assert_json:return', status: :pass, message: 'return all data with provided key', data: { 
+        provided_key: key 
+      }
+      return data[key]
+    else
+      self.fail! kind: 'assert_json:return', status: :pass, message: 'failed to return data with provided key since key does not exist', data: {
+        provided_key: key 
+      }
+    end
+  end
+
+  def pass! kind:, status:, message:, data: {}
+    @report.pass!(
+      describe_key: @describe_key, 
+      spec_key: @spec_key,
+      kind: kind,
+      status: status,
+      message: message,
+      data: data
+    )
+  end
+
+  def fail! kind:, status:, message:, data: {}
+    @report.pass!(
+      describe_key: @describe_key, 
+      spec_key: @spec_key,
+      kind: kind,
+      status: status,
+      message: message,
+      data: data
+    )
   end
 end
