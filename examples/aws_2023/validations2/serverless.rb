452c0f9ec643da2546ddbfef095eeff5ee5fcc43
Andrew Brown <andrew@monsterboxpro.com>
2023-06-27 18:36:56 +0000

    setup serverless

--- examples/aws_2023/validations2/serverless.rb
+++ examples/aws_2023/validations2/serverless.rb
@@ -1,2 +1,20 @@
-Cpbvt::Tester::Runner.describe :serverless do |t|
+Cpbvt::Tester::Runner.describe :serverless do
+  spec :should_have_key_upload_route do |t|
+    
+  end
+
+  spec :should_have_proxy_route do |t|
+  end
+
+  spec :should_have_s3_bucket_with_event_notification do |t|
+  end
+
+  spec :should_block_public_access_for_assets_bucket do |t|
+  end
+
+  spec :should_have_a_cloudfront_distrubition_to_assets do |t|
+  end
+
+  spec :should_have_route53_to_distribution_for_assets do |t|
+  end
 end
