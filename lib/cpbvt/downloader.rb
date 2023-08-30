533f075492f982b44d4d7d10bfec38967f4847d0
Andrew <andrew@exampro.co>
2023-08-29 21:43:58 -0400

    Downloader should not break current code

--- lib/cpbvt/downloader.rb
+++ lib/cpbvt/downloader.rb
@@ -7,16 +7,17 @@ class Cpbvt::Downloader
                aws_access_key_id:,
                aws_secret_access_key:,
                payloads_bucket:)
-    Aws.config.update({
-      credentials: Aws::Credentials.new(
-        aws_access_key_id,
-        aws_secret_access_key
-      )
-    })
-    # Create an S3 client
-    s3_client = Aws::S3::Client.new
+    #Aws.config.update({
+    #  credentials: Aws::Credentials.new(
+    #    aws_access_key_id,
+    #    aws_secret_access_key
+    #  )
+    #})
+    ## Create an S3 client
+    #s3_client = Aws::S3::Client.new
 
-    File.open(file_path, 'wb') do |file|
-      s3.get_object({ bucket: payloads_bucket, key: object_key }, target: file)
-    end
+    #File.open(file_path, 'wb') do |file|
+    #  s3.get_object({ bucket: payloads_bucket, key: object_key }, target: file)
+    #end
+  end
 end
--- lib/cpbvt/version.rb
+++ lib/cpbvt/version.rb
@@ -1,3 +1,3 @@
 module Cpbvt
-  VERSION = "1.2.2"
+  VERSION = "1.2.3"
 end
