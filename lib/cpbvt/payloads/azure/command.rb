685d7de6c9a5513f1c754e36273f4a0dfb7f7ff5
Andrew <andrew@exampro.co>
2023-11-19 11:19:08 -0500

    Put back my example json so I can see the data structure.

--- lib/cpbvt/payloads/azure/command.rb
+++ lib/cpbvt/payloads/azure/command.rb
@@ -17,6 +17,23 @@ class Cpbvt::Payloads::Azure::Command
       stdout_str, exit_code = Open3.capture2(command)#, :stdin_data=>post_content)
       puts "\nSTDOUT_STR =="
       puts stdout_str
+      # Example output expected to be returned
+      #[
+      #  {
+      #    "cloudName": "AzureCloud",
+      #    "homeTenantId": "f73244ae-3e74-43eb....",
+      #    "id": "7f3352cf-6c7d-456a-8ecb....",
+      #    "isDefault": true,
+      #    "managedByTenants": [],
+      #    "name": "Azure subscription",
+      #    "state": "Enabled",
+      #    "tenantId": "f73244ae-3e74-43eb-8dbf....",
+      #    "user": {
+      #      "name": "19dc2af0-a12c-493a-8ac5....",
+      #      "type": "servicePrincipal"
+      #    }
+      #  }
+      #]
       result = JSON.parse(stdout_str)
       #result = payload['Credentials']
       # TODO - check the result
