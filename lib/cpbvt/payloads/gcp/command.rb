978f470758587a38e6ac69d3ff417ade6f596ba7
bayko <bayko7@gmail.com>
2023-11-22 15:31:23 -0500

    wip

--- lib/cpbvt/payloads/gcp/command.rb
+++ lib/cpbvt/payloads/gcp/command.rb
@@ -4,12 +4,11 @@ class Cpbvt::Payloads::Gcp::Command
   include Cpbvt::Payloads::Gcp::Commands::Storage
 
   def self.login gcp_key_file
-command = <<~COMMAND
-gcloud auth activate-service-account \
---key-file=#{gcp_key_file}
-COMMAND
-puts "[Executing] #{command}"
-
+      command = <<~COMMAND
+      gcloud auth activate-service-account \
+      --key-file=#{gcp_key_file}
+      COMMAND
+      puts "[Executing] #{command}"
       begin
         stdout_str, exit_code = Open3.capture2(command)#, :stdin_data=>post_content)
         payload = JSON.parse(stdout_str)
